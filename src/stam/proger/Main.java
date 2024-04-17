a3d8d504cfb8eb0d1478b21ae5030f3748c0d95f
rusyun8 <rustam.yunusov@attolabs.de>
2024-04-17 13:57:09 +0500

    first commit

---
+++ src/stam/proger/Main.java
@@ -0,0 +1,10 @@
+package stam.proger;
+
+public class Main {
+
+    public static void main(String[] args){
+        ContactForm form = new ContactForm();
+        form.setVisible(true);
+    }
+
+}
